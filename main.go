659ad44b977e3178f3d4b1669f162bb991f1ba91
Mark Dietzer <git@doridian.net>
2021-02-07 19:33:18 -0800

    Basic relay WS server

---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+var socketLock sync.RWMutex
+var sockets map[string]*websocket.Conn
+
+func sendTo(target string, msg []byte) {
+	socketLock.RLock()
+	socket := sockets[target]
+	socketLock.RUnlock()
+	if socket == nil {
+		return
+	}
+	socket.WriteMessage(websocket.TextMessage, msg)
+}
+
+func broadcast(msg []byte) {
+	socketLock.RLock()
+	for _, socket := range sockets {
+		go socket.WriteMessage(websocket.TextMessage, msg)
+	}
+	socketLock.RUnlock()
+}
+
+type identResp struct {
+	Name string `json:"name"`
+}
+
+func getIdent(w http.ResponseWriter, r *http.Request) string {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.spaceage.mp/v2/servers/self", nil)
+	if err != nil {
+		w.WriteHeader(400)
+		return ""
+	}
+	req.Header.Add("Authorization", r.Header.Get("Authorization"))
+	resp, err := client.Do(req)
+	if err != nil {
+		w.WriteHeader(400)
+		return ""
+	}
+
+	if resp.StatusCode != 200 {
+		w.WriteHeader(resp.StatusCode)
+		return ""
+	}
+
+	var respData identResp
+	err = json.NewDecoder(resp.Body).Decode(&respData)
+	resp.Body.Close()
+	if err != nil {
+		w.WriteHeader(500)
+		return ""
+	}
+
+	return respData.Name
+}
+
+func wshandler(w http.ResponseWriter, r *http.Request) {
+	ident := getIdent(w, r)
+	if ident == "" {
+		return
+	}
+
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	socketLock.Lock()
+	oldC := sockets[ident]
+	if oldC != nil {
+		delete(sockets, ident)
+		go oldC.Close()
+	}
+	socketLock.Unlock()
+
+	defer func() {
+		socketLock.Lock()
+		if sockets[ident] == c {
+			delete(sockets, ident)
+		}
+		socketLock.Unlock()
+	}()
+	defer c.Close()
+
+	for {
+		decoded := make(map[string]interface{})
+		err := c.ReadJSON(&decoded)
+		if err != nil {
+			break
+		}
+		decoded["ident"] = ident
+
+		encoded, err := json.Marshal(decoded)
+
+		target, ok := decoded["target"].(string)
+		if ok {
+			go sendTo(target, encoded)
+		} else {
+			go broadcast(encoded)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws/central", wshandler)
+	err := http.ListenAndServe("127.0.0.1:9888", nil)
+	if err != nil {
+		panic(err)
+	}
+}
